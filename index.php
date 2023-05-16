1dad9d2432bd45f0c10df6911a8009df4617eb31
Jérémy Lorette <jeremy.lorette@outlook.com>
2023-05-16 12:00:42 +0200

    structure projet

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./public/css/style.css">
-    <script defer src="./public/js/index.js"></script>
+    <?php require_once 'includes/head.php' ?>
     <title>Todo</title>
 </head>
 
 <body>
-    <header>HEADER</header>
-    <div class="content">CONTENT</div>
-    <footer>FOOTER</footer>
+    <div class="container">
+        <?php require_once 'includes/header.php' ?>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <?php require_once 'includes/footer.php' ?>
+    </div>
 </body>
 
 </html>
